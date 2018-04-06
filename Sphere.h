326ebd4f3d5daa90ff88532e0fe893ff2cab38d5
Austin Atchley <austinatchley@gmail.com>
2018-04-06 04:19:48 -0500

    Implemented Sphere::intersect

--- Sphere.h
+++ Sphere.h
@@ -15,9 +15,26 @@ struct Sphere {
       : center(c), radius(r), radius2(r * r), surface_color(sc),
         transparency(trans), reflection(refl) {}
 
-  bool intersect(const float3 &r_orig, const float3 &r_dir, float &h0,
-                 float &h1) const {
-    // detect intersection here, set h0, h1 if they exist
+  bool intersect(const float3 &r_orig, const float3 &r_dir, float &t0,
+                 float &t1) const {
+    // draw a line between the center of the sphere and ray origin
+    float3 line = center - r_orig; 
+
+    // tca is the vector of line projected onto r_dir
+    float tca = line.dot(r_dir);
+    if (tca < 0) // the ray is going in the wrong direction
+      return false;
+
+    float dist2 = line.dot(line) - tca * tca;
+    if (dist2 < radius2) // dist is too short to reach the sphere
+      return false;
+
+    // to get the radius of intersection, compute how much
+    // of r_dir is in the sphere
+    float rad_of_inter = sqrt(radius2 - dist2);
+    t0 = tca - rad_of_inter;
+    t1 = tca + rad_of_inter;
+    
     return true;
   }
 };
