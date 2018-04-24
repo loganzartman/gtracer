f2fab908a2a1bfcc21921a01b99be99d0d854b43
Austin Atchley <austinatchley@gmail.com>
2018-04-23 23:34:15 -0500

    Added loader.hh

---
+++ loader.hh
@@ -0,0 +1,14 @@
+#ifndef LOADER_HH
+#define LOADER_HH
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "Vec3.hh"
+
+
+std::vector<float3> load(std::string filename);
+
+#endif
